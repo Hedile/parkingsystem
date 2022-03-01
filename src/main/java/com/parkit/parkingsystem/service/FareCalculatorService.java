27959cceb7b768000326bc3092394605250c6bd3
hedil <chedil@yahoo.fr>
2022-03-01 21:32:27 +0100

    Ajout fonctionnalité de stationnement gratuit pour 30 premières minutes

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -13,18 +13,21 @@ public class FareCalculatorService {
 		double inHour = ticket.getInTime().getTime();
 		double outHour = ticket.getOutTime().getTime();
 		double duration = (outHour - inHour) / (60 * 60 * 1000);
-
-		switch (ticket.getParkingSpot().getParkingType()) {
-		case CAR: {
-			ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-			break;
-		}
-		case BIKE: {
-			ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-			break;
-		}
-		default:
-			throw new IllegalArgumentException("Unkown Parking Type");
+		if (duration <= 0.5) { // si duration est inférieure à 30 minutes, le coût est gratuit pour le client
+			ticket.setPrice(0);
+		} else {
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+				break;
+			}
+			case BIKE: {
+				ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+			}
 		}
 	}
 }
